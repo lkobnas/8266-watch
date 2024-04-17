453365cffb234955b9c1fed399695125e1f992b8
lkobnas <lkobnas@gmail.com>
2024-04-17 22:54:08 +0800

    Added init

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,19 @@
 #include <Arduino.h>
 #include "PageController.h"
+#include "Display.h"
 
 // put function declarations here:
 int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
-  int result = myFunction(2, 3);
-  Serial.println(result);
-  PageController pageController;
-  pageController.nextPage();
+  Serial.begin(115200);
+
+  //init
+
+  Display* display = new Display(0x3C);
+  PageController* pageController = new PageController(display);
+  pageController->nextPage();
 
 }
 
